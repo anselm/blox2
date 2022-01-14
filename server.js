ee50e8da04f2fa2f0e94f74af7af25d963b5d554
Anselm Hook <anselm@gmail.com>
2022-01-13 16:38:19 -0800

    port 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = 5000
+const port = 80
 
 app.use(express.static('public'))
 
